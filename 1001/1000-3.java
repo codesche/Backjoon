bbe0db45f1e12ffb53a4810f43bcf239272e00c5
Minsung <110509654+codesche@users.noreply.github.com>
2022-09-19 16:33:46 +0900

    Update 1000-3.java

--- 1001/1000-3.java
+++ 1001/1000-3.java
@@ -1,18 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-// BufferedReader, StringTokenizer 활용
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();                // readline 사용시 main메소드에 throws IOException 없으면 에러발생
-        StringTokenizer st = new StringTokenizer(str, " ");
-        int numA = Integer.parseInt(st.nextToken());
-        int numB = Integer.parseInt(st.nextToken());
+        String[] str = br.readLine().split(" ");
 
-        System.out.println(numA + numB);
+        // 숫자입력시 기존 String형에서 int형으로 변환
+        int numA = Integer.parseInt(str[0]);
+        int numB = Integer.parseInt(str[1]);
+
+        System.out.println(numA -numB);
     }
 }
