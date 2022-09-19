8e09a3c7799eb0267847054d1d7e341f24e66c15
Minsung <110509654+codesche@users.noreply.github.com>
2022-09-19 16:32:21 +0900

    Create 1001-2(기본).java

---
+++ 1001/1001-2(기본).java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numA = sc.nextInt();
+        int numB = sc.nextInt();
+
+        System.out.println(numA + numB);
+        sc.close();
+    }
+}
