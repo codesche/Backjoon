d90e354da5614ca03d030c30f4c66288b3ecc3e9
Minsung <110509654+codesche@users.noreply.github.com>
2022-09-20 16:15:34 +0900

    Create Solution.java

---
+++ 1330/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sc.close();
+
+        if (a > b) {
+            System.out.print('>');
+        } else if (a < b) {
+            System.out.print('<');
+        } else if (a == b) {
+            System.out.print("==");
+        }
+    }
+}
