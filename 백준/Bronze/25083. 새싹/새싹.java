03e791185822183168e37b880acb18c669ecad7e
Minsung <110509654+codesche@users.noreply.github.com>
2022-09-23 00:23:46 +0900

    [Bronze V] Title: 새싹, Time: 128 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "         ,r'\"7\n" + 	
+                "r`-_   ,'  ,/\n" + 		
+                " \\. \". L_r'\n" + 		
+                "   `~\\/\n" + 			
+                "      |\n" + 			
+                "      |";
+
+        System.out.print(s);
+   
+    }
+}
