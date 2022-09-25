74a262ca0b5022491d29db7f02dd1f50560e1b91
Minsung <110509654+codesche@users.noreply.github.com>
2022-09-25 22:36:33 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14720 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        double arr[] = new double[Integer.parseInt(br.readLine())];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Double.parseDouble(st.nextToken());
+        }
+        
+        double sum = 0;
+        
+        // 정렬 - 끝요소가 최대값
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < arr.length; i++) {
+            sum += ((arr[i]) / arr[arr.length - 1] * 100);
+        }
+        System.out.print(sum / arr.length);
+    }
+}
