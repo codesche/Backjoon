b99fad5fb3d5ba552aa0cf2dddaeb971f0df28cf
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-01 13:46:24 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.println(a > b ? a : b);
+    }
+}
