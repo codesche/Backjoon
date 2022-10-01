14888262cc7e43065b2b3257be1deb03a3909be4
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-01 17:02:19 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 13.85 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 0; i < strings.length; i++) {
+            list.add(strings[i].charAt(n) + strings[i]);
+        }
+
+        // 정렬
+        Collections.sort(list);
+
+        answer = new String[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i).substring(1, list.get(i).length());
+        }
+
+        return answer;
+    }
+}
