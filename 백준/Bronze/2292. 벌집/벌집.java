23dc74abb4ab257ac83791e620ee3a50d6436174
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-02 19:10:08 +0900

    [Bronze II] Title: 벌집, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 1;      // 최소 루트
+        int range = 2;      // 범위
+
+        if (N == 1) {
+            System.out.print(1);
+        } else {
+            while (range <= N) {                        // 범위가 N보다 커지기 직전까지 반복
+                range = range + (6 * count);
+                count++;
+            }
+            System.out.print(count);
+        }
+    }
+}
+
