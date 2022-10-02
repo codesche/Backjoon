64d2e5657677994942b591483516cf229ee11db2
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-02 19:45:55 +0900

    [Bronze I] Title: 분수찾기, Time: 148 ms, Memory: 16064 KB -BaekjoonHub

---
+++ 백준/Bronze/1193. 분수찾기/분수찾기.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // T : 분자 + 분모
+        int X = Integer.parseInt(br.readLine());
+        int cross_count = 1;            // 대각선 원소의 개수
+        int prev_count_sum = 0;         // 1부터 (T-1) 대각선 까지의 원소 개수
+
+        while (true) {
+            // 직전 대각선 누적합 + 해당 대각선 개수 이용한 범위 판별
+            if (X <= prev_count_sum + cross_count) {
+                if (cross_count % 2 == 1) {             // 대각선의 개수가 홀수라면
+                    // 분자가 큰 수부터 시작
+                    // 분자 = 대각선상 내의 블럭 개수 - (X번째 - 직전 대각선까지의 블럭 개수 - 1)
+                    // 분모는 X 번째 - 직전 대각선까지의 블럭 개수
+                    System.out.print((cross_count - (X - prev_count_sum - 1)) + "/" + (X - prev_count_sum));
+                    break;
+                }
+
+                else {          // 대각선 상의 블럭의 개수가 짝수인 경우
+                    //홀수일 때의 출력을 반대로
+                    System.out.print((X - prev_count_sum) + "/" + (cross_count - (X - prev_count_sum - 1)));
+                    break;
+                }
+            } else {
+                prev_count_sum += cross_count;
+                cross_count++;
+            }
+        }
+    }
+}
