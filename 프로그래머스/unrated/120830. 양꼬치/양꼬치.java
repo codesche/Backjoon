2022c74e1271e7016150f7259941153d920d4789
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-07 20:51:25 +0900

    [unrated] Title: 양꼬치, Time: 0.01 ms, Memory: 66.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        answer = (12000 * n) + (2000 * k) - (2000 * (n / 10));
+        
+        return answer;
+    }
+}
