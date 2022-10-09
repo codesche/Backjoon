3ead3d7f6908aeded749c035fbd4689840a76f43
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-09 23:29:52 +0900

    [unrated] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        
+        if (n % slice != 0) {
+            answer = 1 + (n / slice);
+        } else if (n % slice == 0) {
+            answer = n / slice;
+        }
+        
+        return answer;
+    }
+}
