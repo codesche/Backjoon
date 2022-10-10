68e26e1aab54bfabbafc7991fbcd8d5f613545ad
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-10 20:20:57 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,29 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        double arr[] = new double[Integer.parseInt(br.readLine())];
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int i = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Double.parseDouble(st.nextToken());
+        while (st.hasMoreTokens()) {
+            A[i] = Integer.parseInt(st.nextToken());
+            i++;
         }
-        
+
+        double max = 0;
         double sum = 0;
-        
-        // 정렬 - 끝요소가 최대값
-        Arrays.sort(arr);
-        
-        for (int i = 0; i < arr.length; i++) {
-            sum += ((arr[i]) / arr[arr.length - 1] * 100);
+
+        for (int j = 0; j < A.length; j++) {
+            if (A[j] > max) {
+                max = A[j];
+            }
+            sum += A[j];
         }
-        System.out.print(sum / arr.length);
+        System.out.print((sum * 100) / max / N);
     }
 }
