77a00b88f8dd905c8c7b320d97cfaaa0707319f5
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-23 00:23:20 +0900

    [Silver IV] Title: 스택, Time: 356 ms, Memory: 18108 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            
+            if (str.indexOf("push") > -1) {
+                int tmp = Integer.parseInt(str.substring(5));
+                stack.push(tmp);
+            }
+
+            switch (str) {
+                case "top":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                    break;
+
+                case "pop":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+                    
+                    break;
+
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+
+                case "empty":
+                    if (stack.isEmpty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
