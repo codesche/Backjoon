35a16c38ce0be6d9fb6c43557a78a68d9b95f29b
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-23 22:37:15 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int count = 0;
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (arr[i] <= K) {
+                count += (K / arr[i]);
+                K = K % arr[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
