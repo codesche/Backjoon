9a7a16b1c51a023cc07ca5f35f5d0adbd099182e
Minsung <110509654+codesche@users.noreply.github.com>
2022-10-23 22:06:12 +0900

    [level 2] Title: 구명보트, Time: 8.75 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int index = 0;
+
+        for (int i = people.length - 1; i >= index; i--) {
+            if (people[i] + people[index] <= limit) {
+                index++;
+                answer++;
+            } else {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
