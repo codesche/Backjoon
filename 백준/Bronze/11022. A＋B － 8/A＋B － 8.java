84603b1d889501f1eb73c9ea7d4050427bcd01fb
codesche <110509654+codesche@users.noreply.github.com>
2022-11-30 14:22:05 +0900

    [Bronze V] Title: A+B - 8, Time: 176 ms, Memory: 16440 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,23 +1,22 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 11022번
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int a;
-        int b;
+        int T = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i < T + 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a + b));
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int sum = A + B;
+            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + sum);
         }
-        br.close();
     }
 }
+
