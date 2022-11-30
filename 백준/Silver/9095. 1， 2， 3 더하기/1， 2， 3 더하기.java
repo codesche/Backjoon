c33557b214c6168f9728a057cfd8e812dac7dbff
codesche <110509654+codesche@users.noreply.github.com>
2022-11-30 23:44:43 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,25 +1,22 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int T = Integer.parseInt(br.readLine());
+        int[] nums = new int[11];
+        nums[1] = 1;
+        nums[2] = 2;
+        nums[3] = 4;
 
         for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
-            int[] DP = new int[11];
-
-            DP[1] = 1;
-            DP[2] = 2;
-            DP[3] = 4;
-
-            for (int j = 4; j <= N; j++) {
-                DP[j] = DP[j - 1] + DP[j - 2] + DP[j - 3];
+            int n = Integer.parseInt(br.readLine());
+            for (int j = 4; j <= n; j++) {
+                nums[j] = nums[j - 1] + nums[j - 2] + nums[j - 3];
             }
-            bw.write(DP[N] + "\n");
+            System.out.println(nums[n]);
         }
-        bw.flush();
     }
 }
