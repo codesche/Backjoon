7e16c079d50611bc7e9e51eca5701b6362897558
codesche <110509654+codesche@users.noreply.github.com>
2022-11-30 22:01:56 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 384 ms, Memory: 20168 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            if (i == N) {
+                for (int j = 1; j <= 2 * i - 1; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+                break;
+            }
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
