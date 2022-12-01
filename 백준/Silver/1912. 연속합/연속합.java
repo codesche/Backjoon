41de0ea981af5ff45e469ba0c4f49f1b50c0ae29
codesche <110509654+codesche@users.noreply.github.com>
2022-12-01 16:55:19 +0900

    [Silver II] Title: 연속합, Time: 276 ms, Memory: 23544 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        /*
+        dp[0]은 첫 원소로 이전에 더이상 탐색할 것이 없기 때문에
+        arr[0] 자체 값이 되므로 arr[0]으로 초기화해줌
+        max 역시 첫 번째 원소로 초기화해줌
+         */
+        dp[0] = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            // (이전 dp + 현재 arr값) 과 현재 arr값 중 큰 것을 dp에 저장
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            // 최댓값 경신
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
