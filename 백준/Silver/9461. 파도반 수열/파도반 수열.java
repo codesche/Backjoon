25fe720f3e993e06712d660f1635eb2e3e22bf3c
codesche <110509654+codesche@users.noreply.github.com>
2022-12-01 19:50:10 +0900

    [Silver III] Title: 파도반 수열, Time: 120 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static long[] seq = new long[101];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        padovan();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            sb.append(seq[Integer.parseInt(br.readLine())]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+
+    public static void padovan() {
+        seq[1] = 1;
+        seq[2] = 1;
+        seq[3] = 1;
+
+        for (int i = 4; i < 101; i++) {
+            seq[i] = seq[i - 2] + seq[i - 3];
+        }
+    }
+}
