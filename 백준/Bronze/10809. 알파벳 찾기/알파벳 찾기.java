29a40211176a8c2a7792094278a64f3a586b26bc
codesche <110509654+codesche@users.noreply.github.com>
2022-12-05 14:59:30 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 144 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -4,27 +4,24 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[26];
         String str = br.readLine();
+        int[] arr = new int[26];
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < 26; i++) {
             arr[i] = -1;
         }
 
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
 
-            // arr 원소 값이 -1인 경우에만 초기화
             if (arr[ch - 'a'] == -1) {
                 arr[ch - 'a'] = i;
             }
         }
 
-        for (int val : arr) {       // 배열 출력
-            System.out.print(val + " ");
+        for (int i = 0; i < 26; i++) {
+            System.out.print(arr[i] + " ");
         }
-
     }
 }
