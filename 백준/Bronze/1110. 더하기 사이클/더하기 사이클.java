26bf5d71d11770ad52c35e4fb1863c1e53e4095d
codesche <110509654+codesche@users.noreply.github.com>
2022-12-08 21:08:29 +0900

    [Bronze I] Title: 더하기 사이클, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -4,18 +4,20 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
         int cnt = 0;
-        int N = Integer.parseInt(br.readLine());
-        int sum = N;
+        int copy = N;
 
         do {
-            N = ((N % 10) * 10 + ((N / 10) + (N % 10)) % 10);
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
             cnt++;
-        } while (sum != N);
 
+            if (copy == N) {
+                break;
+            }
+        } while (copy != N);
         System.out.println(cnt);
     }
 }
