253439647a2139f8e74ea42d82740d1a8dc24d22
codesche <110509654+codesche@users.noreply.github.com>
2022-12-08 17:23:04 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -2,13 +2,12 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 answer += i;
-            } else {
-                continue;
             }
         }
+        
         return answer;
     }
 }
