c79c3ee9b083b00b300ffaa905d494269cbff719
codesche <110509654+codesche@users.noreply.github.com>
2022-12-09 11:19:30 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
-        int sum = 0;
-
+        
         String str = String.valueOf(x);
-
-        for (int i = 0; i < str.length(); i++) {
-            sum += str.charAt(i) - '0';
+        char[] ch = str.toCharArray();
+        int sum = 0;
+        
+        for (int i = 0; i < ch.length; i++) {
+            sum += ch[i] - '0';
         }
         
         if (x % sum == 0) {
-            answer = true;
+            return answer;
         } else {
-            answer = false;
+            return false;
         }
-
-        return answer;
     }
 }
