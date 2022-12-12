42b5babca57a11f9980229f37091df7c17af6163
codesche <110509654+codesche@users.noreply.github.com>
2022-12-13 00:59:10 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,10 +1,14 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 45;
-        
-        for (int n : numbers) {
-            answer -= n;
+        int answer = 0;
+        int sum = 45;
+        int temp_sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            temp_sum += numbers[i];
         }
+        
+        answer = sum - temp_sum;
         
         return answer;
     }
