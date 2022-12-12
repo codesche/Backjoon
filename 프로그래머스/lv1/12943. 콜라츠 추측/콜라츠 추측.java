4f3c86c207a08d7a9779b8a0ae66d6986c0a074b
codesche <110509654+codesche@users.noreply.github.com>
2022-12-12 23:50:55 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,22 +1,26 @@
 class Solution {
     public int solution(int num) {
-        long number = num;
-        int cnt = 0;
+        int answer = 0;
+        long n = num;
 
-        while (number != 1) {
-            if (number % 2 == 0) {
-                number /= 2;
-            } else  {
-                number = (number * 3) + 1;
+        if (n == 1) {
+            return 0;
+        }
+
+        while (n != 1) {
+            if (Math.abs(n) % 2 == 0) {
+                n = Math.abs(n / 2);
+            } else if (Math.abs(n) % 2 != 0) {
+                n = (Math.abs(n * 3)) + 1;
             }
 
-            cnt++;
+            answer++;
 
-            if (cnt > 500) {
+            if (answer > 500) {
                 return -1;
             }
         }
 
-        return cnt;
+        return answer;
     }
 }
