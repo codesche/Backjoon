b7d570a245e7a2a00dab801f745298cb6c484a86
codesche <110509654+codesche@users.noreply.github.com>
2023-01-09 09:57:26 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello");
+        sb.append(" World!");
+        System.out.println(sb);
+    }
+}
