da9ed39d0f3e5f926aad05acf2205ed738fe8e19
codesche <110509654+codesche@users.noreply.github.com>
2023-01-09 10:00:18 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|\n");
+        
+        System.out.println(sb);
+    }
+}
