0b62975bdf2d6f200147ee48fd5ac5590981d892
codesche <110509654+codesche@users.noreply.github.com>
2023-01-10 11:36:38 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+        int sum = 0;
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - (arr[i] + arr[j]) == 100) {
+                    arr[i] = 0;
+                    arr[j] = 0;
+                    Arrays.sort(arr);
+                    for (int k = 2; k < 9; k++) {
+                        System.out.println(arr[k]);
+                    }
+                    return;
+                }
+            }
+        }
+
+    }
+}
