3fcb06f4b5789a524ed34fe4de990f5fff5bef1b
codesche <110509654+codesche@users.noreply.github.com>
2023-01-11 13:05:44 +0900

    [Bronze III] Title: 최댓값, Time: 140 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,22 +1,32 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        List<Integer> list = Arrays.asList(sc.nextInt(), sc.nextInt(), sc.nextInt(),
-                sc.nextInt(), sc.nextInt(), sc.nextInt(),
-                sc.nextInt(), sc.nextInt(), sc.nextInt());
-        
-        sc.close();
-        
-        int val = Collections.max(list);
-        int idx = list.indexOf(val) + 1;
 
-        System.out.println(val);
-        System.out.println(idx);
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int idx = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                idx = i + 1;
+            }
+        }
+
+        sb.append(max + "\n");
+        sb.append(idx);
+
+        System.out.println(sb);
+
     }
+
 }
