7173642edd23c72bfd5db7741c104507e55f886e
codesche <110509654+codesche@users.noreply.github.com>
2023-01-14 11:58:22 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 568 ms, Memory: 295120 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,40 +1,37 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+        Queue<Integer> queue = new LinkedList();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        Queue<Integer> queue = new LinkedList<>();
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
         for (int i = 1; i <= N; i++) {
-            queue.add(i);
+            queue.offer(i);
         }
 
         StringBuilder sb = new StringBuilder();
-        sb.append("<");
+        sb.append('<');
 
-        // Queue의 사이즈가 1일 때까지 반복
-        while (queue.size() != 1) {
+        while (queue.size() > 1) {
+
             for (int i = 0; i < K - 1; i++) {
-                queue.offer(queue.poll());
+                int val = queue.poll();
+                queue.offer(val);
             }
-            // K번째 값은 poll한 후 출력
-            sb.append(queue.poll() + ", ");
+
+            sb.append(queue.poll()).append(", ");
+
         }
 
-        // Queue의 사이즈가 1일 때는 단순히 poll하면 됨
-        sb.append(queue.poll() + ">");
+        sb.append(queue.poll()).append('>');
+        System.out.println(sb);
 
-        bw.write(sb.toString() + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
     }
+
 }
