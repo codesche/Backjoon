2129d7f518917cf1ced5d6a00365e16292d8e88a
codesche <110509654+codesche@users.noreply.github.com>
2023-01-14 12:37:04 +0900

    [Silver IV] Title: 제로, Time: 236 ms, Memory: 24204 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,29 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Integer> stack = new Stack<Integer>();
+
         int K = Integer.parseInt(br.readLine());
 
+        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < K; i++) {
-            int N = Integer.parseInt(br.readLine());
+            int value = Integer.parseInt(br.readLine());
 
-            if (N == 0) {
+            if (value == 0) {
                 stack.pop();
             } else {
-                stack.push(N);
+                stack.push(value);
             }
+
         }
         
-        int sum = 0;
-
-        for (int o : stack) {
-            sum += o;
+        int answer = 0;
+        for (int i = 0; i < stack.size(); i++) {
+            answer += stack.get(i);
         }
-        System.out.println(sum);
+
+        System.out.println(answer);
     }
+
 }
