9e7f023237b51d57a33edcde8f7bcb55ed58090c
codesche <110509654+codesche@users.noreply.github.com>
2023-01-15 16:33:16 +0900

    [Silver II] Title: 스택 수열, Time: 340 ms, Memory: 27372 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,58 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();         // 출력할 결과물 저장
+        Stack<Integer> stack = new Stack<>();
 
         int N = Integer.parseInt(br.readLine());
-        int arr[] = new int[N];
 
+        int start = 0;
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+            int value = Integer.parseInt(br.readLine());
 
-        Stack<Integer> stack = new Stack<>();
-        StringBuffer sb = new StringBuffer();
-
-        // 오름차순 수
-        int num = 1;
-        boolean result = true;
-
-        for (int i = 0; i < arr.length; i++) {
-            int sequence_value = arr[i];
-
-            // 현재 수열 값 >= 오름차순 자연수 : 값이 같아질 때까지 push() 수행
-            if (sequence_value >= num) {
-                while (sequence_value >= num) {
-                    stack.push(num++);
-                    sb.append("+\n");
+            if (value > start) {
+                for (int j = start + 1; j <= value; j++) {
+                    stack.push(j);
+                    sb.append('+').append('\n');
                 }
-                stack.pop();
-                sb.append("-\n");
+                start = value;                  // 다음 push 할 때의 오름차순을 유지하기 위한 변수 초기화
             }
 
-            // 현재 수열 값 < 오름차순 자연수 : pop()을 수행하여 수열 원소를 꺼낸다
-            else {
-                int n = stack.pop();
-
-                // 스택의 가장 위의 수가 만들어야 하는 수열의 수보다 클 경우 수열을 출력할 수 없음
-                if (n > sequence_value) {
-                    System.out.println("NO");
-                    result = false;
-                    break;
-                }
-
-                else {
-                    sb.append("-\n");
-                }
+            // top에 있는 원소가 입력받은 값과 같지 않은 경우
+            else if (stack.peek() != value) {
+                System.out.println("NO");
+                return;
             }
+
+            stack.pop();
+            sb.append('-').append('\n');
         }
-        if (result) {
-            System.out.println(sb.toString());
-        }
+
+        System.out.println(sb);
+
     }
+
 }
