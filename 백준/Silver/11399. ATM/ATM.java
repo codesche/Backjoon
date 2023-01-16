d84be59533cb445dd83afb614b6d07616100b5c0
codesche <110509654+codesche@users.noreply.github.com>
2023-01-16 10:54:09 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,51 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        int[] S = new int[N];
 
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int prev = 0;       // 이전까지 대기시간 누적합
+        int sum = 0;        // 사람별 대기시간 총합
 
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            sum += prev + arr[i];
+
+            prev += arr[i];
         }
 
-        // 삽입 정렬
-        for (int i = 1; i < N; i++) {
-            int insert_point = i;
-            int insert_value = A[i];
-            for (int j = i - 1; j >= 0; j--) {
-                if (A[j] < A[i]) {
-                    insert_point = j + 1;
-                    break;
-                }
-
-                if (j == 0) {
-                    insert_point = 0;
-                }
-            }
-
-            for (int j = i; j > insert_point; j--) {
-                A[j] = A[j - 1];
-            }
-            A[insert_point] = insert_value;
-        }
-
-        S[0] = A[0];                                // 합 배열 만들기
-
-        for (int i = 1; i < N; i++) {
-            S[i] = S[i - 1] + A[i];
-        }
-        int sum = 0;                                // 합 배열 총합 구하기
-        for (int i = 0; i < N; i++) {
-            sum = sum + S[i];
-        }
         System.out.println(sum);
     }
+
 }
