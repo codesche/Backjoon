ca5e248e25c3cde6cc49365e8aae047cfed8a753
codesche <110509654+codesche@users.noreply.github.com>
2023-01-27 12:21:15 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,27 +1,29 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int result = 0;
-
-        int[] arr = new int[N];
+        int[] coin = new int[N];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            coin[i] = Integer.parseInt(br.readLine());
         }
 
-        for (int i = N - 1; i >= 0; i--) {
-            if (arr[i] <= K) {                  // 현재 동전의 가치가 K보다 작거나 같으면 구성에 추가
-                result += (K / arr[i]);
-                K = K % arr[i];                 // K를 현재 동전을 사용하고 남은 금액으로 갱신
+        int count = 0;
+
+        for (int i = coin.length - 1; i >= 0; i--) {
+            if (K >= coin[i]) {
+                count += (K / coin[i]);
+                K = K % coin[i];
             }
         }
-        System.out.println(result);
+        System.out.println(count);
     }
+
 }
