aa796f4f59c5921fe61fb9992ff55ab554180794
codesche <110509654+codesche@users.noreply.github.com>
2023-03-13 23:08:58 +0900

    [Silver I] Title: 회의실 배정, Time: 512 ms, Memory: 43608 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,38 +1,46 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        int prev_end_time = 0;
         int N = Integer.parseInt(br.readLine());
+
         int[][] time = new int[N][2];
 
-        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            time[i][0] = Integer.parseInt(st.nextToken());      // 시작
-            time[i][1] = Integer.parseInt(st.nextToken());      // 종료
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            time[i][0] = Integer.parseInt(st.nextToken());          // 시작시간
+            time[i][1] = Integer.parseInt(st.nextToken());          // 종료시간
         }
 
-        Arrays.sort(time, (o1, o2) -> {
-            if (o1[1] == o2[1]) {           // 종료 시간이 같을 경우 시작 시간이 빠른 순
-                return o1[0] - o2[0];
+        // 끝나는 시간 기준으로 정렬 위해 compare를 재정의
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+
+                // 종료시간 같으면 시작시간이 빠른순으로 정렬
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+
+                return o1[1] - o2[1];
             }
-            return o1[1] - o2[1];
         });
 
-        int cnt = 0;
-        int end = 0;
-
         for (int i = 0; i < N; i++) {
-            if (end <= time[i][0]) {
-                end = time[i][1];
-                cnt++;
+            // 직전 종료시간이 다음 회의 시작 시간보다 작거나 같을 경우 업데이트
+            if (prev_end_time <= time[i][0]) {
+                prev_end_time = time[i][1];
+                count++;
             }
         }
-        System.out.println(cnt);
+        System.out.println(count);
     }
-
 }
