720a24fc6c6f4e49120cc40f46623d14ef36e703
codesche <110509654+codesche@users.noreply.github.com>
2023-03-14 17:25:30 +0900

    [level 2] Title: 위장, Time: 0.06 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+
+        for (String key : map.keySet()) {
+            answer *= (map.get(key) + 1);           // (A + 1) * (B + 1)
+        }
+
+        answer -= 1;                                // 상의, 하의 안 입을 경우 고려
+        return answer;
+    }
+}
