522c6f62b4e2c4c9b8c1a058e014e8238f79733c
codesche <110509654+codesche@users.noreply.github.com>
2023-03-29 12:28:52 +0900

    [Silver III] Title: N과 M (2), Time: 236 ms, Memory: 18348 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int[] arr;
+    static int N, M;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        N = in.nextInt();
+        M = in.nextInt();
+
+        arr = new int[M];
+
+        dfs(1, 0);
+        System.out.print(sb);
+    }
+
+    public static void dfs(int at, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = at; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
+
+}
