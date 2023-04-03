c45f9537c1ece3a491f337f2077a81acad1a5a24
codesche <110509654+codesche@users.noreply.github.com>
2023-04-03 13:01:26 +0900

    [Gold V] Title: 수 고르기, Time: 348 ms, Memory: 27704 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,17 +1,15 @@
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
         int[] arr = new int[N];
 
         for (int i = 0; i < N; i++) {
@@ -20,28 +18,23 @@ public class Main {
 
         Arrays.sort(arr);
 
+        int result = Integer.MAX_VALUE;
         int i = 0;
         int j = 0;
-        int answer = Integer.MAX_VALUE;
 
-        // 투 포인터 알고리즘
+
         while (i < N) {
             if (arr[i] - arr[j] < M) {
                 i++;
                 continue;
             }
-
             if (arr[i] - arr[j] == M) {
-                answer = M;
+                result = M;
                 break;
             }
-
-            answer = Math.min(answer, arr[i] - arr[j]);
+            result = Math.min(result, arr[i] - arr[j]);
             j++;
         }
-
-        System.out.println(answer);
-
+        System.out.println(result);
     }
-
 }
