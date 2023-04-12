c392de14d7f597e6f45e28c0fe79bf0431f27922
codesche <110509654+codesche@users.noreply.github.com>
2023-04-12 13:56:02 +0900

    [level 0] Title: 모스부호 (1), Time: 1.50 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,24 @@
+
+public class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        String[] morseList = {".-", "-...", "-.-.", "-..",
+                ".", "..-.", "--.", "....", "..", ".---",
+                "-.-", ".-..", "--", "-.", "---", ".--.", 
+                "--.-", ".-.", "...", "-", "..-", "...-", 
+                ".--", "-..-", "-.--", "--.."};
+        
+        String[] morse;
+        
+        morse = letter.split(" ");
+        for (String s : morse) {
+            for (int i = 0; i < morseList.length; i++) {
+                if (s.equals(morseList[i])) {
+                    answer += Character.toString(i + 'a');
+                }
+            }
+        }
+
+        return answer;
+    }
+}
