502a59b93c004b912bb556bea2cae49680e9e692
codesche <110509654+codesche@users.noreply.github.com>
2023-04-19 13:02:21 +0900

    [Silver V] Title: 방 번호, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,52 +1,34 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        String str = br.readLine();
+        int[] arr = new int[10];
 
-        int N = Integer.parseInt(br.readLine());
-
-        int[] plastic = new int[9];
-
-        int cnt = 0;
-
-        // 일의 자리부터 N의 자리까지 탐색
-        while (N != 0) {
-            int temp = N % 10;
-
-            if (temp == 9) {                // 9는 6과 동일
-                temp = 6;
-            }
-
-            if (plastic[temp] == 0) {       // 현재 가지고 있는 플라스틱 숫자의 개수가 0이라면 새로 구입
-                buySet(plastic);
-                plastic[temp]--;
-                cnt++;
+        for (int i = 0; i < str.length(); i++) {
+            int num = Character.getNumericValue(str.charAt(i));
+            if (num == 6) {
+                arr[9]++;
             } else {
-                plastic[temp]--;
+                arr[num]++;
             }
-
-            N /= 10;
         }
 
-        bw.write(cnt + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-
-    }
-
-    static void buySet(int[] plastic) {
+        int max = 0;
         for (int i = 0; i < 9; i++) {
-            if (i == 6) {
-                plastic[i] += 2;
-            } else {
-                plastic[i] += 1;
-            }
+            max = Math.max(max, arr[i]);
         }
-    }
 
+        int nine = arr[9] / 2;
+        if (arr[9] % 2 == 1) {
+            nine++;
+        }
+
+        max = Math.max(max, nine);
+        System.out.print(max);
+
+    }
 }
