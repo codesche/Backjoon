95efbae32cf6fe778d348c9341e1dcee993dbb6e
codesche <110509654+codesche@users.noreply.github.com>
2023-04-25 14:15:49 +0900

    [unrated] Title: rny_string, Time: 0.04 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+        
+        answer = rny_string.replace("m", "rn");
+        
+        return answer;
+    }
+}
