1b5dcacfa360178c2204d85720378d3bf2199bc2
codesche <110509654+codesche@users.noreply.github.com>
2023-05-02 14:24:05 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,10 @@
+
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        
+        answer = Integer.toString(Integer.parseInt(n_str));
+        
+        return answer;
+    }
+}
